f1e66f48069fd5c88336087f7f21e27bca23dfb8
jadilsonl12 <jadilson1995@hotmail.com>
2025-01-03 21:00:21 -0300

    Chore: Criação do banco de dados, da tabela de estabelecimentos e da funções de insert, findAll e findById

---
+++ app/src/main/java/com/example/monitoring/database/SampleData.kt
@@ -0,0 +1,33 @@
+package com.example.monitoring.database
+
+import com.example.monitoring.domain.Establishments
+
+val sampleDataEstablishments = listOf(
+    Establishments(
+        categoryId = "12",
+        name = "Supermercado Central",
+        description = "O melhor supermercado da região.",
+        coupons = 10,
+        address = "Av. Paulista, 1234 - São Paulo, SP",
+        phone = "(11) 98765-4321",
+        urlImage = "https://www.nutrimixassessoria.com.br/wp-content/uploads/2021/08/BOAS-PRATICAS-EM-SUPERMERCADOS.jpg"
+    ),
+    Establishments(
+        categoryId = "102",
+        name = "Mercadinho da Esquina",
+        description = "Uma experiência única de compras.",
+        coupons = 15,
+        address = "Rua das Flores, 567 - Rio de Janeiro, RJ",
+        phone = "(21) 91234-5678",
+        urlImage = "https://blog.ciss.com.br/wp-content/uploads/2021/09/blog-ciss-sortimento-de-produtos.jpg"
+    ),
+    Establishments(
+        categoryId = "103",
+        name = "Sabor Grill",
+        description = "Churrascaria com cortes nobres e buffet variado. Experiência completa para os amantes de carnes.",
+        coupons = 5,
+        address = "Rua das Flores, 567 - Rio de Janeiro, RJ",
+        phone = "(21) 91234-5678",
+        urlImage = "https://images.unsplash.com/photo-1498654896293-37aacf113fd9?w=400&h=300"
+    )
+)
