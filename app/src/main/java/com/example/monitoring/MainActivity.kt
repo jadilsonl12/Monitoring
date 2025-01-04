234822d4d1921f83080238be5a55c7bc5f7f3fa6
jadilsonl12 <jadilson1995@hotmail.com>
2025-01-03 23:02:05 -0300

    Feat: Atualizando a MainActivity com as telas de navegação

--- app/src/main/java/com/example/monitoring/MainActivity.kt
+++ app/src/main/java/com/example/monitoring/MainActivity.kt
@@ -3,15 +3,22 @@ package com.example.monitoring
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.activity.enableEdgeToEdge import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.nearby.ui.theme.MonitoringTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.monitoring.ui.routes.Details
+import com.example.monitoring.ui.screen.home.HomeScreen
+import com.example.monitoring.ui.screen.market_details_screen.MarketDetailsScreen
+import com.example.monitoring.ui.screen.splash.SplashScreen
+import com.example.monitoring.ui.screen.welcome.WelcomeScreen
+import com.example.monitoring.ui.routes.Home
+import com.example.monitoring.ui.routes.Register
+import com.example.monitoring.ui.routes.Splash
+import com.example.monitoring.ui.routes.Welcome
+import com.example.monitoring.ui.screen.register.RegisterScreen
+import com.example.monitoring.ui.theme.MonitoringTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +26,69 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MonitoringTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Splash
+                ) {
+                    composable<Splash> {
+                        SplashScreen(
+                            onNavigateToWelcome = {
+                                navController.navigate(Welcome)
+                            }
+                        )
+                    }
+                    composable<Welcome> {
+                        WelcomeScreen(
+                            onNavigateToHome = {
+                                navController.navigate(Home)
+                            }
+                        )
+                    }
+                    composable<Register> {
+                        RegisterScreen(
+                            onNavigateBack = {
+                                navController.popBackStack()
+                            }
+                        )
+                    }
+                    composable<Home> {
+                        HomeScreen(
+                            onNavigateToRegister = {
+                                navController.navigate(Register)
+                            },
+                            onNavigateToDetails = {
+                                navController.navigate(Details)
+                            }
+                        )
+                    }
+                    composable<Details> {
+                        MarketDetailsScreen(
+                            onNavigateBack = {
+                                navController.popBackStack()
+                            }
+                        )
+                    }
+//                    composable<Market> {
+//                        val selectedMarket = it.toRoute<Market>()
+//
+//                        MarketDetailsScreen(
+//                            market = selectedMarket,
+//                            onNavigateBack = {
+//                                navController.popBackStack()
+//                            }
+//                        )
+//                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MonitoringTheme {
-        Greeting("Android")
+
     }
 }
