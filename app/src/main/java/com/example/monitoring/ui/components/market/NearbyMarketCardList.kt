98f1443e690caca6fd826b304df7fb3d3f5dc46a
jadilsonl12 <jadilson1995@hotmail.com>
2025-01-03 23:14:07 -0300

    Fix: Adicionando a importação correta do função "items"

--- app/src/main/java/com/example/monitoring/ui/components/market/NearbyMarketCardList.kt
+++ app/src/main/java/com/example/monitoring/ui/components/market/NearbyMarketCardList.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
